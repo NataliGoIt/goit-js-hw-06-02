2cd8eb3a4f1986d31104dc32562a128328c0ce3d
NataliGoIt <nataliakal4enko@gmail.com>
2023-01-25 17:56:13 +0200

    9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,7 +1,6 @@
 const btnChandgeCokor = document.querySelector(".change-color"),
   spanColor = document.querySelector(".color"),
   body = document.querySelector("body");
-console.log(body);
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
@@ -11,5 +10,4 @@ function changeColor() {
   const hexColor = getRandomHexColor();
   spanColor.textContent = hexColor;
   body.style.backgroundColor = hexColor;
-  console.log(spanColor);
 }
